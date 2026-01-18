0417601de0537af4e91ff94bed0258f8099cfb52
Arun Rar <arunrar2024@outlook.com>
2026-01-18 14:18:38 +0530

    Initialized React frontend structure

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+      <div style={{ padding: "20px", fontFamily: "Arial" }}>
+        <h2>Password Vault Frontend</h2>
+        <p>React app is running.</p>
+      </div>
+  );
+}
+
+export default App;
