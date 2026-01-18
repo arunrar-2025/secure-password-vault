57cc76b8038f19035a9147995737062fed6509c9
Arun Rar <arunrar2024@outlook.com>
2026-01-18 15:26:39 +0530

    CORS fixed

--- backend/public/index.php
+++ backend/public/index.php
@@ -4,6 +4,14 @@
     declare(strict_types=1);
 
     header("Content-Type: application/json");
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        http_response_code(200);
+        exit;
+    }
 
     session_start();
 
